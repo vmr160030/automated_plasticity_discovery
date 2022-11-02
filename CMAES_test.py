4ad5f09cd1729f48d25a15331080164c15c7f379
David Bell <davidgrebell@gmail.com>
2022-11-02 11:24:17 -0700

    check loss function can be minimized; introduce simpler cases of perturbation; remove exc recurrence

--- CMAES_test.py
+++ CMAES_test.py
@@ -22,7 +22,7 @@ T = 0.1
 dt = 1e-4
 t = np.linspace(0, T, int(T / dt))
 n_e = 15
-n_i = 10
+n_i = 20
 
 if not os.path.exists('sims_out'):
 	os.mkdir('sims_out')
@@ -36,23 +36,27 @@ layer_colors = get_ordered_colors('winter', 15)
 r_in = generate_gaussian_pulse(t, 0.005, 0.005, w=1)
 
 transfer_e = partial(tanh, v_th=0.1)
-transfer_i = partial(tanh, v_th=0.5)
+transfer_i = partial(tanh, v_th=0.1)
 
 plasticity_coefs = np.zeros(81)
 
-w_e_e = 4e-4 / dt
-w_e_i = 1e-4 / dt
-w_i_e = -2.5e-5 / dt
+w_e_e = 0.8e-3 / dt
+w_e_i = 0.5e-4 / dt
+w_i_e = -0.25e-4 / dt
 
 all_w_initial = []
 
 for i in range(N_NETWORKS):
 	w_initial = np.zeros((n_e + n_i, n_e + n_i))
-	w_initial[:n_e, :n_e] = np.where(
-		np.diag(np.ones(n_e - 1), k=-1) > 0,
-		gaussian_if_under_val(1.01, (n_e, n_e), w_e_e, 0.3 * w_e_e),
-		exp_if_under_val(1.01, (n_e, n_e), 0.1 * w_e_e)
-	)
+	w_initial[:n_e, :n_e] = w_e_e * np.diag( 0.8 * np.log10(np.arange(n_e - 1) + 10), k=-1)
+	w_initial[:n_e, :n_e] = w_initial[:n_e, :n_e] * (0.1 + 0.9 *np.random.rand(n_e, n_e))
+
+	# np.where(
+	# 	np.diag(np.ones(n_e - 1), k=-1) > 0,
+	# 	gaussian_if_under_val(1.01, (n_e, n_e), w_e_e, 0 * w_e_e),
+	# 	0
+	# 	#exp_if_under_val(1.01, (n_e, n_e), 0.1 * w_e_e)
+	# )
 
 	w_initial[n_e:, :n_e] = gaussian_if_under_val(1.01, (n_i, n_e), w_e_i, 0.3 * w_e_i)
 	w_initial[:n_e, n_e:] = gaussian_if_under_val(1.01, (n_e, n_i), w_i_e, 0.3 * np.abs(w_i_e))
@@ -62,14 +66,15 @@ for i in range(N_NETWORKS):
 # Defining L2 loss and objective function
 
 r_target = np.zeros((len(t), n_e))
-period = 6e-3
+delay = 3.75e-3
+period = 10e-3
 offset = 2e-3
 
 for i in range(n_e):
-	active_range = (period * i + offset, period * (i+1) + offset)
+	active_range = (delay * i + offset, delay * i + period + offset)
 	n_t_steps = int(period / dt)
 	t_step_start = int(active_range[0] / dt)
-	r_target[t_step_start:(t_step_start + n_t_steps), i] = np.sin(np.pi/period * dt * np.arange(n_t_steps))
+	r_target[t_step_start:(t_step_start + n_t_steps), i] = 0.25 * np.sin(np.pi/period * dt * np.arange(n_t_steps))
 
 def l2_loss(r, r_target):
 	if np.isnan(r).any():
@@ -85,7 +90,7 @@ def simulate_single_network(w_initial, plasticity_coefs):
 	w = copy(w_initial)
 
 	for i in range(N_INNER_LOOP_ITERS):
-		r, s, v, w_out = simulate_seq(t, n_e, n_i, r_in, transfer_e, transfer_i, plasticity_coefs, w, dt=dt, tau_e=5e-3, tau_i=0.1e-3, g=1, w_u=1)
+		r, s, v, w_out = simulate_seq(t, n_e, n_i, r_in, transfer_e, transfer_i, plasticity_coefs, w, dt=dt, tau_e=10e-3, tau_i=0.1e-3, g=1, w_u=1)
 		# dw_aggregate = np.sum(np.abs(w_out - w))
 		if np.isnan(r).any():
 			return r, w
@@ -103,11 +108,11 @@ def simulate_plasticity_rules(plasticity_coefs, eval_tracker=None):
 	results = pool.map(f, all_w_initial)
 	pool.close()
 
-	loss = np.sum([l2_loss(res[0], r_target) for res in results]) + 1000 * np.sum(np.abs(plasticity_coefs))
+	loss = np.sum([l2_loss(res[0], r_target) for res in results]) + 5 * N_NETWORKS * np.sum(np.abs(plasticity_coefs))
 
 	if eval_tracker is not None:
 		scale = 1
-		fig, axs = plt.subplots(2, 2, sharex=False, sharey=False, figsize=(10 * scale, 3  * scale))
+		fig, axs = plt.subplots(2, 2, sharex=False, sharey=False, figsize=(14 * scale, 6  * scale))
 
 		r, w = results[-1]
 
@@ -128,6 +133,8 @@ def simulate_plasticity_rules(plasticity_coefs, eval_tracker=None):
 		zero_padding = '0' * pad
 		evals = eval_tracker['evals']
 
+
+		fig.tight_layout()
 		fig.savefig(f'{out_dir}/{zero_padding}{evals}.png')
 
 		eval_tracker['best_loss'] = loss
@@ -150,6 +157,6 @@ options = {
 	'verb_filenameprefix': os.path.join(out_dir, 'outcmaes/'),
 }
 
-x, es = cma.fmin2(partial(simulate_plasticity_rules, eval_tracker=eval_tracker), x0, 0.0025, options=options)
+x, es = cma.fmin2(partial(simulate_plasticity_rules, eval_tracker=eval_tracker), x0, 0.01, options=options)
 print(x)
 print(es.result_pretty())
